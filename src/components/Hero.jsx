4c388e9bd7c3ff49b280def6842ab9e052392c89
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-09-29 12:16:16 +0100

    Navbar redo complete, Hero component section complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,29 +4,27 @@ import styles from '../style'
 
 const Hero = () => {
   return (
-    <section className="bg-[url('./assets/heroImg.png')] bg-no-repeat bg-top sm:h-full h-fit">
+    <section className="bg-[url('./assets/heroImg2.png')] lg:bg-[url('./assets/heroImg.png')] h-screen bg-cover bg-center">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
               <Navbar />
           </div>
       </div>
       
-      <div id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div id="home" className={` md:bg-transparent flex md:flex-row flex-col ${styles.paddingY} absolute bottom-0 left-0`}>
         <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-          <h1 className="font-Lato font-normal ss:text-[68px] text-[48px] text-dimBlack ss:leading-[100px] leading-[58px]">
-            Hello, <br className='sm:block hidden' />
+          <h1 className="font-Lato font-bold ss:text-[68px] text-[24px] lg:text-[48px] text-[#F2F2F2] md:text-dimBlack ss:leading-[100px] leading-[28.8px] md:leading-[58px]">
+            Hello, <br/>
             I am Paul Amusan
           </h1>
-          <p className={`${styles.paragraph} max-w-[467px] mt-5 text-[16px] text-dimBlack font-Lato font-normal`}>
+          <p className={`${styles.paragraph} max-w-[467px] mt-5 text-12px lg:text-[16px] text-[#F2F2F2] md:text-dimBlack font-Lato font-light leading-[18px] md:leading-[24px]`}>
             Paul Amusan is a young, energetic, and talented fashion designer who focuses on his men’s clothing line, Paul Amusan. His work draws inspiration from local Nigerian fabrics coupled with Japanese tailoring designs. His designs are masculine, yet modern and trendy by way of transforming an ordinary look into a sophisticated design that embraces elegance.
           </p>
-          <button type='button' className={`mt-[32px] border border-dimBlack py-4 px-6 w-[189px] h-[74px] font-Lato font-normal text-[18px] text-dimBlack leading-[21px] outline-none} ${styles}`}>
+          <button type='button' className={`mt-[32px] border border-dimBlack py-4 px-6 w-[189px] h-[74px] font-Lato font-normal text-[18px] text-dimBlack leading-[21px] outline-none} ${styles} sm:block hidden`}>
             Hire Me
           </button>
         </div>
       </div>
-
-      
 
     </section>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const NavBar = () => {
       <div className='sm:hidden flex flex-1 justify-start items-center'>
           <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={() => setToggle((prev) => !prev)} />
 
-          <div className={`${toggle ? "flex" : "hidden"} p-6 bg-dimBlack absolute top-20 left-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
+          <div className={`${toggle ? "flex" : "hidden"} p-6 bg-dimBlack absolute top-20 left-0 mx-4 my-2 min-w-[320px] rounded-xl sidebar `}>
             <ul className="list-none flex flex-col justify-end items-center flex-1">
               {navLinks.map((nav, index) =>(
                 <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-grey ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4  ' }`} >
