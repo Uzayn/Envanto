7818f159fc71897f8d42de90fd552ad01787e662
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-10-17 13:52:00 +0100

    It's a wrap

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,33 +12,33 @@ const Contacts = () => {
         <p className='flex justify-center text-[10px] md:text-[14px] font-Lato font-normal leading-[21px] text-dimBlack'>Need anything? Kindly fill the form below</p>
       </div>
 
-      <div className={`${styles.paddingX} ${styles.paddingY} flex`}>
+      <div className={`${styles.paddingY} flex justify-center`}>
         <div className='mr-[125px] hidden md:flex'>
           <img src={aboutImg} alt="" />
         </div>
-        <div className='font-Lato font-normal text-[24px] leading-[21px] w-fit overflow-hidden'>
+        <div className='font-Lato font-normal text-[24px] leading-[21px] w-full md:w-fit ml-[20px] overflow-hidden'>
           <div className='md:flex'>
             <div className='font-Lato font-normal text-dimBlack text-[14px] md:text-[24px] leading-[21px]'>Name</div>
             <div>
-              <input type="text" className='border border-grey rounded bg-transparent w-[360px] md:w-[505px] h-[40px] md:h-[60px] md:ml-[42px] mb-[16px] md:mb-[40px]' />
+              <input type="text" className='border border-grey rounded bg-transparent w-full md:w-[505px] h-[40px] md:h-[60px] md:ml-[42px] mb-[16px] md:mb-[40px]' />
             </div>
           </div>
           <div className='md:flex'>
             <div className='font-Lato font-normal text-dimBlack text-[14px] md:text-[24px] leading-[21px]'>Email</div>
             <div>
-              <input type="text" className='border border-grey rounded bg-transparent w-[360px] md:w-[505px] h-[40px] md:h-[60px] md:ml-[47px] mb-[16px] md:mb-[40px]' />
+              <input type="text" className='border border-grey rounded bg-transparent w-full md:w-[505px] h-[40px] md:h-[60px] md:ml-[47px] mb-[16px] md:mb-[40px]' />
             </div>
           </div>
           <div className='md:flex'>
             <div className='font-Lato font-normal text-dimBlack text-[14px] md:text-[24px] leading-[21px]'>Subject</div>
             <div>
-              <input type="text" className='border border-grey rounded bg-transparent w-[360px] md:w-[505px] h-[40px] md:h-[60px] md:ml-[27px] mb-[16px] md:mb-[40px]' />
+              <input type="text" className='border border-grey rounded bg-transparent w-full md:w-[505px] h-[40px] md:h-[60px] md:ml-[27px] mb-[16px] md:mb-[40px]' />
             </div>
           </div>
           <div className='md:flex'>
             <div className='font-Lato font-normal text-dimBlack text-[14px] md:text-[24px] leading-[21px]'>Message</div>
             <div>
-              <input type="text" className='border border-grey rounded bg-transparent w-[360px] md:w-[505px] h-[141px] md:h-[228px] md:ml-[12px] mb-[16px] md:mb-[71px]' />
+              <input type="text" className='border border-grey rounded bg-transparent w-full md:w-[505px] h-[141px] md:h-[228px] md:ml-[12px] mb-[16px] md:mb-[71px]' />
             </div>
           </div>
           <button className='border-[1px] border-grey w-[243px] md:w-[295px ] h-[60px] text-[18px] font- py-[20px] px-[90px] md:ml-[105px]'>
